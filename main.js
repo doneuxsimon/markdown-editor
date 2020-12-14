64993c2771ea2637762dd13bf05462d39791fe2d
doneux simon <doneuxsimon@gmail.com>
2020-12-14 16:29:47 +0100

    prend en compte le texte mais ne le traduis pas

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const textEditor = document.querySelector('.text-editor');
+
+const preview = document.querySelector('.preview');
+
+const converter = new showdown.Converter();
+
+textEditor.addEventListener('keyup', evt => {
+    const { value } = evt.target;
+
+    const html = converter.makeHtml(value);
+
+    preview.innerHTML = html;
+})
